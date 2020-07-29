6d2fca80c64e6f09955fefe039aa0725e3524af7
morozov603v1@gmail.com <morozov603v1@gmail.com>
2020-07-29 18:26:21 +0700

    Добавьте файлы проекта.

---
+++ INTMA-test/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace INTMA_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGrid1 = new System.Windows.Forms.DataGridView();
+            this.dataGrid2 = new System.Windows.Forms.DataGridView();
+            this.dataGrid3 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGrid1
+            // 
+            this.dataGrid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid1.Location = new System.Drawing.Point(21, 26);
+            this.dataGrid1.Name = "dataGrid1";
+            this.dataGrid1.Size = new System.Drawing.Size(217, 145);
+            this.dataGrid1.TabIndex = 0;
+            // 
+            // dataGrid2
+            // 
+            this.dataGrid2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid2.Location = new System.Drawing.Point(352, 26);
+            this.dataGrid2.Name = "dataGrid2";
+            this.dataGrid2.Size = new System.Drawing.Size(217, 145);
+            this.dataGrid2.TabIndex = 1;
+            // 
+            // dataGrid3
+            // 
+            this.dataGrid3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid3.Location = new System.Drawing.Point(352, 271);
+            this.dataGrid3.Name = "dataGrid3";
+            this.dataGrid3.Size = new System.Drawing.Size(217, 145);
+            this.dataGrid3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(107, 191);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = ".csv - datagridview";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(409, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = ".csv - database";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(409, 439);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = " database - .csv ";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(711, 493);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGrid3);
+            this.Controls.Add(this.dataGrid2);
+            this.Controls.Add(this.dataGrid1);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGrid1;
+        private System.Windows.Forms.DataGridView dataGrid2;
+        private System.Windows.Forms.DataGridView dataGrid3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ INTMA-test/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace INTMA_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridBind();
+            InsertDataTableToDB();
+            DbToCsv();
+        }
+
+        public DataTable CsvToDataTable(string fileName)
+        {
+            DataTable result = new DataTable();
+            FileInfo fileInfo = new FileInfo(fileName);
+            string tableName = fileInfo.Name;
+            string folderName = fileInfo.DirectoryName;
+
+            string connectionString = "Provider=Microsoft.Jet.OleDb.4.0;" +
+                                      "Data Source=" + folderName + ";" +
+                                      "Extended Properties=\"Text;" +
+                                      "HDR=YES;" +
+                                      "FMT=Delimited\"";
+
+            using (OleDbConnection connection =
+                new OleDbConnection(connectionString))
+            {
+                connection.Open();
+                string sqlStatement = "SELECT * FROM " + tableName;
+
+                using (OleDbDataAdapter adapter =
+                    new OleDbDataAdapter(sqlStatement, connection))
+                {
+                    result = new DataTable(tableName);
+                    adapter.Fill(result);
+                }
+            }
+
+            return result;
+        }
+
+        private void InsertDataTableToDB()
+        {
+
+            SqlConnection myConnection = new SqlConnection(GetConnStr());
+            string filePath = "C:\\file2.csv";
+
+            DataTable table = CsvToDataTable(filePath);
+            string tableName = "dbo.Table_3";
+            myConnection.Open();
+
+            using (SqlBulkCopy bulkcopy = new SqlBulkCopy(myConnection))
+            {
+                foreach (DataColumn col in table.Columns)
+                    bulkcopy.ColumnMappings.Add(col.ColumnName, col.ColumnName);
+
+                bulkcopy.DestinationTableName = tableName;
+
+                try
+                {
+                    bulkcopy.WriteToServer(table);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+            }
+
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM " + tableName + " ", myConnection);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGrid2.DataSource = dt;
+            myConnection.Close();
+
+        }
+
+        private void DbToCsv()
+        {
+            SqlConnection myConnection = new SqlConnection(GetConnStr());
+            string filePath = "C:\\Users\\Radevich\\Desktop\\file3.csv";
+
+            string tableName = "dbo.Table_4";
+
+            myConnection.Open();
+
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM " + tableName + " ", myConnection);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGrid3.DataSource = dt;
+            myConnection.Close();
+
+            StringBuilder sb = new StringBuilder();
+
+            IEnumerable<string> columnNames = dt.Columns.Cast<DataColumn>().
+                                              Select(column => column.ColumnName);
+
+            sb.AppendLine(string.Join(",", columnNames));
+
+            foreach (DataRow row in dt.Rows)
+            {
+                IEnumerable<string> fields = row.ItemArray.Select(field => field.ToString());
+                sb.AppendLine(string.Join(",", fields));
+            }
+
+            File.WriteAllText(filePath, sb.ToString());
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //string filePath = "C:\\file2.csv";
+
+            //dataGrid1.DataSource = OpenCsvFileAsDataTable(filePath, true);
+        }
+
+        private void dataGridBind()
+        {
+            string filePath = "C:\\Users\\Radevich\\Desktop\\file2.csv";
+
+            dataGrid1.DataSource = CsvToDataTable(filePath);
+        }
+
+
+        private static string GetConnStr()
+        {
+            return "server=МОРОЗОВ-ПК\\SQLEXPRESS;" +
+                "Trusted_Connection=yes;" +
+                "database=test; " +
+                "connection timeout=30";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
